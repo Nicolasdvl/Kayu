41429c2b0d0787a75c619ffddf74767b3ae5055a
Nicolas <nicolasdvl@vivaldi.net>
2023-08-30 18:54:12 +0200

    Implement SearchFilter ; Override filter : query_param 'fields' can specify search_fields

--- app/product/views.py
+++ app/product/views.py
@@ -2,12 +2,25 @@
 from rest_framework import generics
 from product.models import Product, Category
 from product.serializers import ProductSerializer
+from rest_framework import filters
 
 
-class ProductList(generics.ListCreateAPIView):
+class CustomSearchFilter(filters.SearchFilter):
+    def get_search_fields(self, view, request):
+        print(request.query_params)
+        if request.query_params.get('fields'):
+            fields = request.query_params.get('fields').split(',')
+            return [field for field in fields if field in [field.name for field in Product._meta.fields]]
+        return super().get_search_fields(view, request)
+
+class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [CustomSearchFilter]
+    search_fields = ['name',]
+
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
